2f9d99b3c7db5d2f9a79e4f93225843a5cf25f66
Daniel Sattler <daniel.sattler@b13.de>
2016-06-27 14:31:29 +0200

    [TASK] update config.js

--- demo/js/config.js
+++ demo/js/config.js
@@ -1,9 +1,7 @@
 require.config({
 	paths: {
 		jquery: '../../bower_components/jquery/dist/jquery',
-		requirejs: '../../bower_components/requirejs/require',
-		isInViewport: '../../src/isInViewport',
-		underscore: '../../bower_components/underscore/underscore'
+		requirejs: '../../bower_components/requirejs/require'
 	},
 	shim: {
 
